202c240a596797a154dc3550ddd56f8d5992375e
Alejandro Canosa <acanosa@users.noreply.github.com>
2016-05-19 00:04:26 -0300

    Add files via upload

--- dao/LoginDao.java
+++ dao/LoginDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.util.List;
 
+import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -58,6 +59,42 @@ public class LoginDao {
 		return objeto;	
 	}
 	
+	public Login traerLogin(String usuario, String clave) throws HibernateException
+	{
+		Login objeto=null;
+		try{
+			iniciaOperacion();
+			objeto= (Login) session.createQuery("from Login l where l.usuario= '" + usuario + "' and l.clave= '" +clave+"'").uniqueResult();
+			//Hibernate.initialize(objeto.getDatosUsuario()); //traigo los datos del usuario
+		}catch(HibernateException he)
+		{
+			manejaExcepcion(he);
+			throw he;
+			
+		}finally{
+			session.close();
+		}
+		return objeto;	
+	}
+	
+	public Login traerLogin(String usuario) throws HibernateException
+	{
+		Login objeto=null;
+		try{
+			iniciaOperacion();
+			objeto= (Login) session.createQuery("from Login l where l.usuario= '" + usuario + "'").uniqueResult();
+			//Hibernate.initialize(objeto.getDatosUsuario()); //traigo los datos del usuario
+		}catch(HibernateException he)
+		{
+			manejaExcepcion(he);
+			throw he;
+			
+		}finally{
+			session.close();
+		}
+		return objeto;	
+	}
+	
 	public void actualizarLogin(Login objeto) throws HibernateException
 	{
 		try{
@@ -88,6 +125,24 @@ public class LoginDao {
 			session.close();
 		}
 		return Logins;
+	}
+
+	public Login traerDatosUsuario(long idLogin) {
+		// TODO Auto-generated method stub
+		Login objeto=null;
+		try{
+			iniciaOperacion();
+			objeto= (Login) session.createQuery("from Login l where l.idLogin=" +idLogin).uniqueResult();
+			Hibernate.initialize(objeto.getDatosUsuario()); //traigo los datos del usuario
+		}catch(HibernateException he)
+		{
+			manejaExcepcion(he);
+			throw he;
+			
+		}finally{
+			session.close();
+		}
+		return objeto;	
 	}
 	
 
